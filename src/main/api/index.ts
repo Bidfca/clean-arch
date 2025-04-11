ca99d4e1cbba518f84434d2806580a35d7b6209e
Bianca Caetano <bruninhakjurugat11@gmail.com>
2025-04-11 20:51:17 -0300

    module-alias support

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,4 +1,7 @@
-import { PersonController } from "../../application/controllers";
+import "../config/module-alias";
+
+// js do not recognize @ as a path alias, so we need to install module-alias
+import { PersonController } from "@/application/controllers";
 
 const personController = new PersonController();
 console.log(personController.getPerson());
