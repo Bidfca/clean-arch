8cfe445fe4669410932f4f2c08e8e38e897a639c
Bianca Caetano <bruninhakjurugat11@gmail.com>
2025-04-11 21:28:13 -0300

    clean up

--- src/application/controllers/person.ts
+++
@@ -1,9 +0,0 @@
-export class PersonController {
-  constructor() {
-    console.log("PersonController initialized");
-  }
-
-  getPerson() {
-    return { name: "John Doe", age: 30 };
-  }
-}
--- src/main/api/index.ts
+++
@@ -1,7 +0,0 @@
-import "../config/module-alias";
-
-// js do not recognize @ as a path alias, so we need to install module-alias
-import { PersonController } from "@/application/controllers";
-
-const personController = new PersonController();
-console.log(personController.getPerson());
