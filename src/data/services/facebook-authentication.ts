1d9af39ef926baaba2495e4ffc49957643cccbfd
Bianca Caetano <bruninhakjurugat11@gmail.com>
2025-04-16 11:56:03 -0300

    ensure FacebookApi get app token with correct url

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -16,8 +16,6 @@ export class FacebookAuthenticationService implements FacebookAuthentication {
     private readonly crypto: TokenGenerator
   ) {}
 
-  // This service should only handle orchestration and delegation.
-  // The logic for creating or updating a user account should be moved to a domain model.
   async perform(
     params: FacebookAuthentication.Params
   ): Promise<FacebookAuthentication.Result> {
@@ -26,45 +24,14 @@ export class FacebookAuthenticationService implements FacebookAuthentication {
       const accountData = await this.userAccountRepo.load({
         email: fbData.email,
       });
-
-      // Instead of instantiating FacebookAccount directly here, we should inject it.
-      // This makes testing easier: we can use a test double for the dependency,
-      // whereas mocking an internally instantiated class requires mocking a whole module.
-      // Also, by testing FacebookAccount separately, we avoid duplicating tests.
-      // Here, we only need to verify that FacebookAccount is called with the correct parameters.
       const fbAccount = new FacebookAccount(fbData, accountData);
       const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount);
       const token = await this.crypto.generateToken({
         key: id,
         expirationInMs: AccessToken.expiresInMs,
       });
-
       return new AccessToken(token);
     }
     return new AuthenticationError();
   }
 }
-
-// This model is too generic and doesn't satisfy the contract required by the saveWithFacebook interface.
-// class UserAccount {
-//   id?: string;
-//   name?: string;
-//   email: string;
-//   facebookId?: string;
-
-//   constructor(model: { id?: string; name?: string; email: string }) {
-//     this.id = model.id;
-//     this.name = model.name;
-//     this.email = model.email;
-//   }
-
-//   updateWithFacebook(model: {
-//     name: string;
-//     email: string;
-//     facebookId: string;
-//   }) {
-//     this.name = model.name;
-//     this.facebookId = model.facebookId;
-//     this.name ??= model.name;
-//   }
-// }
